f157444ecbae1ffb6e79779eed6d9c412d406365
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-16 18:21:00 +0900

    fix: downcase private_key

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -59,7 +59,7 @@ class ContractsController < ApplicationController
     if key.private_key.nil?
       Tapyrus::Key.new(pubkey: key.public_key.to_bn.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
     else
-      Tapyrus::Key.new(priv_key: key.private_key.to_s(16).encode('US-ASCII'), key_type: 0)
+      Tapyrus::Key.new(priv_key: key.private_key.to_s(16).downcase.encode('US-ASCII'), key_type: 0)
     end
   end
 end
