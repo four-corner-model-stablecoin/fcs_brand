59976bfa10258ad62c7812d6c143b951205c9463
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-16 19:02:44 +0900

    fix: did has_one key

--- app/models/did.rb
+++ app/models/did.rb
@@ -1,4 +1,5 @@
 class Did < ApplicationRecord
+  has_one :key
   has_many :contract_as_brand, class_name: 'Contract', foreign_key: 'brand_did_id'
   has_many :contract_as_issuer, class_name: 'Contract', foreign_key: 'issuer_did_id'
 
