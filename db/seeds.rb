5d537f92191124265ee2f1f9d4563ae1505496e2
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-10-29 15:13:16 +0900

    chore: update dids in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-brand_did = Did.create!(short_form: "did:ion:EiC_pmuSjG4VMd61m-ScQkg7YPVVX6-Mo9Fn_cMGZmPoJw")
+brand_did = Did.create!(short_form: "did:ion:EiCZI36gD-gRcHwJGAppxi6bXQfnEGoVD8bAqPt4k4kjRw:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJzaWduaW5nLWtleSIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJUY0U4RmtlMUtIelFnRXd2X1pmNTRQLUJNLUR1bEtIYW9KUHEtVV9Fc184IiwieSI6IkpMaG1YbTR6WVo1T1kxX1JKcUdza3FpREE0M1doMFR5ekRxeTUwR29Va2MifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOltdfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlBVGpZU3Mzb0VmdExZTmstT2paODlKNjMwQ2lFZmVSNU1OY3E0RG9BQ21EdyJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpRHlJaTVVZGZRWHd3dnVqLXQ1ZUZQeWtnX1lSZTZjZnVYenAySjBNSGsxR1EiLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaURNRkR1Y0VGNUpaUGYzV25xLWhVS09nREl4ektjUXpfRzBEeUR1VmlabnF3In19")
 jwk = {
   "kty": "EC",
   "crv": "secp256k1",
-  "x": "Sp1zTvtooTckKLn1e5mZ1lRyOruVZen918HW3HQxbFc",
-  "y": "OKGyUbaELSi4fbu0CeqkMkZEMrbjyDxx1cvQIcSO0Kw",
-  "d": "zxHazfJ2WAiPsk6I16ek0jwv1hzqxXLDyrZqQCIMrB4"
+  "x": "TcE8Fke1KHzQgEwv_Zf54P-BM-DulKHaoJPq-U_Es_8",
+  "y": "JLhmXm4zYZ5OY1_RJqGskqiDA43Wh0TyzDqy50GoUkc",
+  "d": "Bbo-XIXn7loQMFejOHs7EgJQ-jctotOMRicWMiLA2BM"
 }
 Key.create!(did: brand_did, jwk: jwk.to_json)
 
